84f295fed7a7ac33b6043e4da96caf73f213dcaa
joycelin12 <joycelin12@hotmail.com>
2018-02-01 11:14:42 -0500

    added the part for createshareforecast intent

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -38,5 +40,13 @@ public class DetailActivity extends AppCompatActivity {
                 .setText(mForecast + FORECAST_SHARE_HASHTAG)
                 .getIntent();
         return shareIntent;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForecastIntent());
+        return true;
     }
 }
